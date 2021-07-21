2dbb3ab0ca78a46c3df9e727805d6d0074037a33
cchan316 <cchan316@gatech.edu>
2021-07-21 07:39:16 -0400

    build login framework

---
+++ README.md
@@ -0,0 +1,7 @@
+# Upollomer
+------
+
+<u>v1.0</u>
+
+This is the init version of Upollomer, built basic framework, user database and REST API
+
--- server.js
+++ server.js
@@ -1,40 +1,68 @@
 // if not in development environment -> do not parse the dotenv file
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
 }
 
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const expressLayouts = require('express-ejs-layouts')
+// npm dependencies
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
+const session = require("express-session");
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy; // strategy can be against local db or google or FB
+const bcrypt = require("bcrypt");
+const flash = require("express-flash")
+const methodOverride = require('method-override')
 
 // default view path
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')  // setting where views are coming from
-app.set('layout', 'layouts/layout')  // all HTML file will follow this customized HTML skeleton layout -> (default) views/layouts/layout.ejs
-app.use(expressLayouts)  // allows to create a layout file for all of HTML
-app.use(express.static('public'))  // where the public files will go (e.g. stylesheets, js)
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views"); // setting where views are coming from
+app.set("layout", "layouts/layout"); // all HTML file will follow this customized HTML skeleton layout -> (default) views/layouts/layout.ejs
+app.use(expressLayouts); // allows to create a layout file for all of HTML
+app.use(express.static("public")); // call public files to HTML or ejs (e.g. stylesheets, js, pics)
+app.use(express.json()); // allow server to accept json as a body for get/post request from REST API (a json API)
 
-// index router
+// Routers
 {
-    const indexRouter = require('./routes/index')
-    app.use('/', indexRouter)
+    app.use("/", require("./routes/index"));
+    app.use("/login", require("./routes/login"));
+    app.use("/register", require("./routes/register"));
+    app.use("/users", require("./routes/users"));
 }
 
-// users database REST API
+// RESTful user database (w/ MVC model)
 {
-    // db connection string/dbName
-    mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    const db = mongoose.connection  // events for connected db
-    db.on('error', (error) => console.error(error))
-    db.once('open', () => console.log('Connected to Database'))
-
-    // middleware -> called between processing the Request and sending the Response
-    app.use(express.json())  // allow server to accept json as a body for get/post request SINCE REST API is a json API
-
-    const usersRouter = require('./routes/users')
-    app.use('/users', usersRouter)  // the stuff about users (after that path) will be related to the router module
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    const db = mongoose.connection; // events for connected db
+    db.on("error", (error) => console.error(error));
+    db.once("open", () => console.log("Connected to Database"));
 }
 
-// for the server to tell us what port its listening to
-app.listen(process.env.PORT || 3000, () => console.log(`Listening at http://localhost:3000`))
+// authentication
+{
+    app.use(express.urlencoded({ extended: false })); // to access forms from request var in post function from express
+    app.use(flash())  // flash message
+    app.use(
+        session({
+        // key that encrypt all information
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        })
+    );
+    // passport.js
+    app.use(passport.initialize());
+    app.use(passport.session()); // save the login session
+    // when log in -> serialize and deserialize users
+    // serialized user will be saved to req.user
+    passport.serializeUser((user, done) => done(null, user.id));
+
+}
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Listening at http://localhost:3000`)
+);
