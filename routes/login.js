ad712c7176e741dd679fc4647671551a14ef8240
cchan316 <cchan316@gatech.edu>
2021-07-21 11:23:48 -0400

    log/reg framework

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('login', { layout: false })
-})
+});
+
+router.post('/', (req, res) => {
+
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -24,45 +24,32 @@ app.use(express.static("public")); // call public files to HTML or ejs (e.g. sty
 app.use(express.json()); // allow server to accept json as a body for get/post request from REST API (a json API)
 
 // Routers
-{
-    app.use("/", require("./routes/index"));
-    app.use("/login", require("./routes/login"));
-    app.use("/register", require("./routes/register"));
-    app.use("/users", require("./routes/users"));
-}
+app.use("/", require("./routes/index"));
+app.use("/login", require("./routes/login"));
+app.use("/register", require("./routes/register"));
+app.use("/users", require("./routes/users"));
 
 // RESTful user database (w/ MVC model)
-{
-    mongoose.connect(process.env.DATABASE_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    const db = mongoose.connection; // events for connected db
-    db.on("error", (error) => console.error(error));
-    db.once("open", () => console.log("Connected to Database"));
-}
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection; // events for connected db
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
 
 // authentication
-{
-    app.use(express.urlencoded({ extended: false })); // to access forms from request var in post function from express
-    app.use(flash())  // flash message
-    app.use(
-        session({
-        // key that encrypt all information
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true,
-        })
-    );
-    // passport.js
-    app.use(passport.initialize());
-    app.use(passport.session()); // save the login session
-    // when log in -> serialize and deserialize users
-    // serialized user will be saved to req.user
-    passport.serializeUser((user, done) => done(null, user.id));
-
-}
-
-app.listen(process.env.PORT || 3000, () =>
-  console.log(`Listening at http://localhost:3000`)
+app.use(express.urlencoded({ extended: false })); // allow forms to post as a req.body -> req.body.${the namefield}
+app.use(flash())  // flash message
+app.use(session({
+    // key that encrypt all information
+    secret: process.env.SESSION_SECRET,
+    resave: false,  // do not delete idle sessions
+    saveUninitialized: false  // empty session objects won't be saved
+    })
 );
+// passport.js
+app.use(passport.initialize());
+app.use(passport.session()); // save the login session
+// when log in -> serialize and deserialize users
+// serialized user will be saved to req.user
+passport.serializeUser((user, done) => done(null, user.id));
+
+app.listen(process.env.PORT || 3000, () => console.log(`Listening at http://localhost:3000`));
